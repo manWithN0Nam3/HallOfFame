da342f48914eb115f70eaf14514348e8b0b9f1c6
Travis Pankoff <Tpankoff94@gmail.com>
2015-05-20 15:00:34 -0500

    Anything

---
+++ HallOfFame/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  HallOfFame
+//
+//  Created by Travis  on 5/20/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ HallOfFame/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  HallOfFame
+//
+//  Created by Travis  on 5/20/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
